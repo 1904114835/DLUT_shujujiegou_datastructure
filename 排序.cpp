30482dff7ddd482282895e4fab5f125809243c8e
Wu Ruilin <44604458+1904114835@users.noreply.github.com>
2019-06-06 20:58:07 +0800

    Update 排序.cpp

--- 排序.cpp
+++ 排序.cpp
@@ -1,6 +1,13 @@
 #include<bits/stdc++.h>
 #define MAX 100
 using namespace std;
+
+
+struct Stack{
+	int data[MAX];
+	int top;
+};
+
 void read(int a[],int n)
 {
 	for(int i=1;i<=n;i++)
@@ -57,7 +64,7 @@ void xiEr(int a[],int n)
 			{
 				a[0]=a[i];a[i]=a[i-d];
 				for(j=i-2*d;j>0&&(a[0] <a[j] );j=j-d )
-				a[j+d]=a[j];
+					a[j+d]=a[j];
 				a[j+d]=a[0];
 			}
 		}
@@ -102,7 +109,72 @@ void kuaiPai_one_direct(int a[], int left, int right) {
 
 //快速排序数组划分
 
+void jdXuanze(int a[],int n)
+{
+	a[0]=0x3f3f3f;
+	int i,j,min;
+	for(i=1;i<=n;i++)
+	{
+		min=0;
+		for(j=i;j<=n;j++)
+		{
+			if(a[j]<=a[min])
+				min=j;
+		}
+		swap(a[i],a[min]);
+	}
+}
 
+void jsPaixu(int a[],int n)
+{	
+	Stack temp[10];
+	int i,j,k=0,max=0,tong;
+	for(i=1;i<n;i++)
+		if(a[i]>max)
+			max=a[i];
+	while(true)
+	{	
+		int qumo=pow(10,k);
+		if(max/qumo==0)break;
+		for(int temp_top_init=0;temp_top_init<=9;temp_top_init++)
+			temp[temp_top_init].top=-1;
+
+		for(i=1;i<=n;i++)
+		{
+			tong=(int)( a[i]/qumo )%10;
+			//printf("%d",tong);
+			//system("pause");
+			temp[tong].data[++temp[tong].top]=a[i];
+		}
+		/*
+		for(int s_t=0;s_t<=9;s_t++)
+		{
+			for(int s_i=0;s_i<=temp[s_t].top;s_i++)
+				printf("%d ",temp[s_t].data[s_i]);
+		}
+		printf("\n");
+		*/
+		j=1;
+		for(tong=0;tong<10;tong++)
+		{
+			int len=temp[tong].top;
+			while(temp[tong].top!=-1 )
+			{
+				a[j]=temp[tong].data[len-temp[tong].top];
+				j++;
+				temp[tong].top--;
+			}
+		}
+		//printf("\n------------\n");
+		k++;
+	}
+}
+
+void duiPaixu(int a[],int n)
+{
+	
+	
+}
 int main()//sqList??不存在的，lenth直接拿n存了 
 {/*--------------数组下标从1开始，0留给哨兵项---------------*/
 	int a[MAX];
@@ -118,11 +190,14 @@ int main()//sqList??不存在的，lenth直接拿n存了
 	}
 	read(a,n);
 	printf("\n\n");
-	//maopao(a,n);
-	//zjcharu(a,n);
-	//xier(a,n);
-	//kuaiPai(a,1,n);
-	kuaiPai_one_direct(a,1,n);
+	//maoPao(a,n);//冒泡
+	//zjChaRu(a,n);//直接插入
+	//xiEr(a,n);//希尔
+	//kuaiPai(a,1,n);//快排（教材版） 
+	//kuaiPai_one_direct(a,1,n);//快排（算法导论版） 
+	//jdXuanze(a,n);//简单选择
+	//jsPaixu(a,n);//基数排序
+	duiPaixu(a,n);//堆排序 
 	read(a,n);
 	return 0;
 }
